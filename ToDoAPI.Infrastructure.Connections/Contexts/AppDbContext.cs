3e080048480cb7ed6004c76a2924d1c2abbb47bd
Grecia Guerrero <greciaguerreroa@gmail.com>
2020-12-02 23:29:41 -0500

    Add migrations

--- ToDoAPI.Infrastructure.Connections/Contexts/AppDbContext.cs
+++ ToDoAPI.Infrastructure.Connections/Contexts/AppDbContext.cs
@@ -9,9 +9,18 @@ namespace ToDoAPI.Infrastructure.Connections.Contexts
     public class AppDbContext: DbContext
     {
         public DbSet<ToDoItem> ToDoItems { get; set; }
-        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder);
 
+            builder.Entity<ToDoItem>().ToTable("todoitems");
+            builder.Entity<ToDoItem>().HasKey(i => i.Id);
+            builder.Entity<ToDoItem>().Property(i => i.Id).IsRequired().ValueGeneratedOnAdd();
+            builder.Entity<ToDoItem>().Property(i => i.Name).IsRequired().HasMaxLength(40);
+            builder.Entity<ToDoItem>().HasIndex(i => i.Name).IsUnique();
         }
     }
 }
---
+++ ToDoAPI.Infrastructure.Connections/Migrations/20201202033806_Initial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace ToDoAPI.Infrastructure.Connections.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "todoitems",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 40, nullable: false),
+                    Completed = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_todoitems", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_todoitems_Name",
+                table: "todoitems",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "todoitems");
+        }
+    }
+}
