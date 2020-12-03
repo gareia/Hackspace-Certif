be8788c14b16740984c014555dcdbde565eed63e
Grecia Guerrero <greciaguerreroa@gmail.com>
2020-12-02 23:32:57 -0500

    Update dtos

--- ToDoAPI.Application.DTO/ToDoItemCreationDTO.cs
+++ ToDoAPI.Application.DTO/ToDoItemCreationDTO.cs
@@ -5,13 +5,9 @@ using System.Text;
 
 namespace ToDoAPI.Application.DTO
 {
-    public class ToDoItemDTO
+    public class ToDoItemCreationDTO
     {
-        [Key]
-        public long Id { get; set; } //int
         [Required]
         public string Name { get; set; }
-        public bool Completed { get; set; } = false;
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
